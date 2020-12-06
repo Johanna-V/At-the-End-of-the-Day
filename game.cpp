6a43f85b8c699b747c4a8bc229cd1f029817b96f
Johanna-V <74023739+Johanna-V@users.noreply.github.com>
2020-12-06 18:43:13 +0100

    Completed "first playable" task

--- game.cpp
+++ game.cpp
@@ -3,6 +3,62 @@
 
 //Definition of the Game class that runs the game
 void Game::run() {
+	std::cout << "Welcome to 'At the End of the Day'! Let's see what our day holds for us. \n";
+
+	//Setting the player location to the start location. 
+	//Player is lowercase here since this is the member of the Game class.
+	//The Game class wraps around the Player (and GameData) struct.
+	player.currentLocation = gameData.getStartLocation();
+
+	//while loop for the game
+	//isRunning is a member of the Game class and simply writing it here sets it to true
+	//since that is what it is in the class
+	while (isRunning) {
+
+		//error handling
+		if (player.currentLocation == nullptr) {
+			std::cout << "Error. Player is not at a valid location! \n";
+			break;
+		}
+
+		//print text for the player's current location
+		std::cout << player.currentLocation->text << "\n\n";
+
+		//if there are no choices for the current location, break the loop and print Game Over
+		if (player.currentLocation->choices.size() == 0) {
+			std::cout << "Game Over! \n";
+			break;
+		}
+
+		std::cout << "Pick an option \n";
+
+		//iterate through the size of the choices vector of the current location
+		//and print the iteration number and the current choice's text
+		for (unsigned int i = 0; i < player.currentLocation->choices.size(); ++i) {
+			std::cout << "[" << i << "] " << player.currentLocation->choices[i].text << "\n";
+		}
+
+		//set a var for the user's input and set it to something outside of what the choice number will be
+		//while the var 'choice' is less than 0 or larger than the total number of choices, 
+		//ask for input to the var 'choice'
+		int choice = -1;
+
+		while (choice < 0 || choice >= player.currentLocation->choices.size()) {
+			std::cin >> choice;
+		}
+
+		//set a var 'nextLocationId' to the locationId in the vector 'choices', that the player chose ('choice' var)
+		//use the gameData function 'getLocationWithId' to search for and get the location with the chosen Id
+		//increment the number of moves the player has made
+		const std::string& nextLocationId = player.currentLocation->choices[choice].locationId;
+		player.currentLocation = gameData.getLocationWithId(nextLocationId);
+		player.moves++;
+	}
+
+	std::cout << "You made " << player.moves << " moves! \n";
+
+	/* De-comment to print all locations
 	std::cout << "Debugging Locations\n";
 	gameData.debugLocations();
+	*/
 }
--- main.cpp
+++ main.cpp
@@ -5,29 +5,7 @@
 Game game;
 
 int main() {
-	//game.run();
 
-	GameData gameData;
-	Location* startLocation = gameData.getStartLocation();
+	game.run();
 
-	if (startLocation == nullptr) {
-		std::cout << "No start location found - something went wrong? \n";
-		return 1;
-	}
-
-	std::cout << "Start Location is " << startLocation->id << ": " << startLocation->text << "\n";
-
-	//If there are more than one item in the choices vector of the startLocation, set a local var to the location id of the first choice in the startLocation
-	//Then set a local var to a specific room id that we provide locally here
-	//If the location we asked for exists, print its text
-	if (startLocation->choices.size() > 0) {
-		const std::string& secondLocationId = startLocation->choices[0].locationId;
-		Location* secondLocation = gameData.getLocationWithId("room2");
-
-		if (secondLocation != nullptr) {
-			std::cout << "Possible Second Location: " << secondLocation->text << "\n";
-		}
-
-		return (0);
-	}
 }
